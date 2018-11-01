507331fd434aeaebb3d1e2ad236fcd20ad12fcae
DESKTOP-3V3G6SP\yslee <dldudtjs101@gmail.com>
2018-11-01 16:52:51 +0900

    base setting

--- RC/rc/rc/views.py
+++ RC/rc/rc/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
+from django.template import loader
+from django.urls.http import HttpResponse
 
 # Create your views here.
 
-<<<<<<< HEAD
 def home(request):
-    return render(request, './home.html', dict({}))
-=======
-from django.http import HttpResponse
-from django.template import loader
-
-
-def index(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('home.html')
     context = {
         'latest_question_list': "test",
     }
     return HttpResponse(template.render(context, request))
->>>>>>> 3980f1f67d7d6b1655dc042a651dfed059c1fef1
