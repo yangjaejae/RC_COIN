e8ef6901dac104c2a03d4c724b7c55d3632653a3
DESKTOP-3V3G6SP\yslee <dldudtjs101@gmail.com>
2018-11-05 17:22:00 +0900

    db commit

--- RC/rc/rc/settings.py
+++ RC/rc/rc/settings.py
@@ -127,14 +127,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# AUTH_USER_MODEL = 'member.User'
-
 # Login setting
 LOGIN_URL = '/accounts/login/'      # default = '/accounts/login/'
 LOGOUT_URL = '/accounts/logout/'    # default = '/accounts/logout/'
 LOGIN_REDIRECT_URL = '/'
 
-
+# Media setting
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
