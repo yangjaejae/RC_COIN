2e1e56da7384fda2fae4464914c8df415506a548
DESKTOP-HMV62QE\under <undertakin@naver.com>
2018-12-12 10:49:30 +0900

    commit

--- RC/rc/api/views.py
+++ RC/rc/api/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from hfc.fabric import Client
 
+cli = Client(net_profile="test/fixtures/network.json")
+
+def get_connection():
+    pass
 ## invoke
 def init_wallet ( id ):
+    print(cli)
     pass
-def publish( id , value ):
+def publish( id, value, date ):
     pass
 def transfer(_from, to, value, type, date):
     pass
