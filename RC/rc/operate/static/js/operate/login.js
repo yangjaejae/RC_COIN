9f531d282d914d6979152b6ea7fb5af34c1fbdd4
DESKTOP-HMV62QE\under <undertakin@naver.com>
2018-12-28 09:32:13 +0900

    login

---
+++ RC/rc/operate/static/js/operate/login.js
@@ -0,0 +1,42 @@
+var isValidated = false;
+
+function modal_setMsg(msg) {
+    // $("#msg").text(msg);
+    if(msg == 'id'){
+        $(".alert_bar_id").show();
+    }else if(msg == 'pass'){
+        $(".alert_bar_pass").show();
+    }else if(msg == 'id_or_pass'){
+        $(".alert_bar_id_or_pass").show();
+    }
+}
+
+function modal_toggle() {
+    $("#myModal").modal("show");
+}
+
+function check_validate() {
+    var username = $("#modalLRInput10").val();
+    var password = $("#modalLRInput11").val();
+    if (username.length < 1) {
+        modal_setMsg('id');
+        // modal_toggle();
+        return false;
+    } else if (password.length < 1) {
+        modal_setMsg('pass');
+        // modal_toggle();
+        return false;
+    }
+    alert(1234)
+    return true;
+}
+
+function login_failed() {
+    if ($("#status").val() == "failed") {
+        modal_setMsg('id_or_pass');
+    }
+}
+
+$(function() {
+    login_failed();
+})
