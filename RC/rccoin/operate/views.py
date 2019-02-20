e66d3458fbe2c117074f98946e4ceab5d5944441
DESKTOP-HMV62QE\under <undertakin@naver.com>
2019-02-20 15:26:57 +0900

    operate/view

--- RC/rccoin/operate/views.py
+++ RC/rccoin/operate/views.py
@@ -361,7 +361,7 @@ def check_length(string, max_len):
     return result
 
 host = "http://210.107.78.166:3000/"
-host2 = "http://210.107.78.167:3000/"
+# host2 = "http://210.107.78.167:3000/"
 
 ## query
 def get_notices():
@@ -457,16 +457,5 @@ def get_default_block():
     get_block_url = host + "get_default_block"
     response = requests.get(get_block_url)
     json_format = json.loads(response.text)
-
-    get_block_url2 = host2 + "get_default_block"
-    response2 = requests.get(get_block_url2)
-    json_format2 = json.loads(response2.text)
-    print(json_format)
-    print("#########################")
-    print(json_format2)
-    result_list = []
-    result_list = json_format + json_format2
-    print("#########################")
-    print(result_list)
-    return result_list
+    return json_format
     
